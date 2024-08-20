bd2cc064869645de2da64a9f58acb37b1d5e7e66
Abhinav shyju <abhinavshyjupc@gmail.com>
2024-08-20 23:19:17 +0530

    Nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import gif from "./assets/test.gif";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [open, setopen] = useState(true);
+  const [open, setOpen] = useState(true);
 
-  if (open) {
-    return (
-      <div className="w-full flex justify-center items-center h-screen bg-black">
-        <img src={gif} alt="" onClick={() => setopen(false)} />
-      </div>
-    );
-  }
-  return <div className="">hy</div>;
+  useEffect(() => {
+    const timer = setTimeout(() => setOpen(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {open ? (
+        <div className="w-full flex justify-center items-center h-screen bg-black">
+          <img src={gif} alt="Loading..." onClick={() => setOpen(false)} />
+        </div>
+      ) : (
+        <div className="h-screen flex justify-center items-center bg-white">
+          <h1>Welcome to the App!</h1>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
